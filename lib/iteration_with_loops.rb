a7abbeb8e9af4f18442b738b0c8f080077713d8d
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-29 23:20:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,6 +4,7 @@ def join_nested_strings(src)
   while rows < src.count do
     elements = 0
     while elements < src[rows].count do
+      if 
       final += src[rows][elements].to_s
       elements += 1
     end
